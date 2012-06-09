f586cd4e2c35c896c25dcb8062963637af796faa
smueller <smueller@dec913bc-ca15-4b49-ae4c-4c4ad41e7e36>
2012-06-09 13:28:55 +0000

    - probe.py:    * Implement Signal class and subclasses PositionSignal, VoltageSignal      and CurrentSignal.    * CurrentSignal: Implement calculation of capacitive pickup.
    git-svn-id: file:///svn_repos/pytokamak.new@4 dec913bc-ca15-4b49-ae4c-4c4ad41e7e36

--- python/probe.py
+++ python/probe.py
@@ -3,7 +3,7 @@ import os
 import h5py
 from mdsclient import *
 
-from matplotlib.pyplot import figure, plot, show
+from matplotlib.pyplot import figure, plot, show, hold
 from tight_figure import tight_figure as figure
 
 from pdb import set_trace
@@ -119,6 +119,65 @@ class IOMds(IO):
         raise NotImplementedError("Saving to MDS not implemented")
 
 
+class Signal:
+    def __init__(self, t, x, name="", type=None):
+        self.t, self.x, self.name, self.type = t, x, name, type
+
+    def trim(self, s):
+        self.t, self.x = self.t[s], self.x[s]
+
+    def norm_to_region(self, cnd):
+        return self.x - self.x[cnd].mean()
+
+    def deriv(self):
+        delta = lambda x: np.r_[x[1]-x[0], x[2:]-x[:-2], x[-1]-x[-2]]
+        dx_dt = delta(self.x)/delta(self.t)
+        return dx_dt
+
+    def plot(self, newfig=True):
+        if newfig: figure()
+        plot(self.t, self.x.T)
+
+
+class PositionSignal(Signal):
+    def __init__(self, t, x, name=""):
+        Signal.__init__(self, t, x, name, 'Position')
+
+    def get_t_ind(self):
+        i0 = np.argmax(self.x)
+        i1 = np.where(self.x[i0:] < self.x[0])[0][0]
+        i1 += i0
+        return i0, i1
+
+
+class VoltageSignal(Signal):
+    def __init__(self, t, x, name=""):
+        Signal.__init__(self, t, x, name, 'Voltage')
+
+
+class CurrentSignal(Signal):
+    def __init__(self, t, x, name="", V=None, C=None):
+        Signal.__init__(self, t, x, name, 'Current')
+        self.V, self.C = V, C
+
+    def capa_pickup(self):
+        cnd = self.t - self.t[0] < 0.05
+        dV_dtc = self.V.deriv()[cnd]
+        N = (dV_dtc*dV_dtc).sum()
+
+        dI = self.norm_to_region(cnd)
+        self.C = (dI[cnd]*dV_dtc).sum()/N
+
+    def I_capa(self):
+        if self.C is None:
+            self.capa_pickup()
+        return self.C*self.V.deriv()
+
+    def I_corr(self):
+        x = self.x - self.I_capa()
+        return CurrentSignal(self.t, x, self.name, self.V, self.C)
+
+
 class Probe:
     def __init__(self, shn=0, sock=None):
         self.shn, self.sock = shn, sock
@@ -126,9 +185,11 @@ class Probe:
         self.IO_mds = self.IO_file = None
         self.nodes = ()
         
+        self.C = None
+
     def mapsig(self):
         pass
-
+        
     def load_mds(self):
         self.x = self.IO_mds.load(self.nodes)
         self.mapsig()
@@ -147,17 +208,23 @@ class Probe:
             self.x = self.IO_mds.load(self.nodes)
             self.save()
 
-        self.mapsig()        
-
-    def cut_tail(self):
-        s = np.where(self.R < 0.99*self.R[0]);
-
-
+        self.mapsig()
 
     def plot(self):
-        X = np.c_[self.I,self.R,self.V/100.]
         figure()
-        plot(self.t,X)
+        hold(True)
+        for S in self.S.itervalues():
+            S.plot(newfig=False)
+
+    def trim(self):
+        for S in self.S.itervalues():
+            if isinstance(S, PositionSignal):
+                i0, i1 = S.get_t_ind()
+                break
+
+        s = slice(i1)
+        for S in self.S.itervalues():
+            S.trim(s)
 
 
 
--- python/probe_aug.py
+++ python/probe_aug.py
@@ -1,22 +1,23 @@
 import numpy as np
 
-from probe import IOMds, IOFile, Probe
+import probe
+reload(probe)
 
-class IOMdsAUG(IOMds):
+class IOMdsAUG(probe.IOMds):
     def __init__(self, *args):
-        IOMds.__init__(self, *args)
+        probe.IOMds.__init__(self, *args)
         self.mdsport = "8001"
         self.mdsfmt = 'augsignal(%d,"LPS","%s","AUGD")'
 
 
-class IOFileAUG(IOFile):
+class IOFileAUG(probe.IOFile):
     def __init__(self, shn=27695):
-        IOFile.__init__(self, shn=shn, subdir="AUG")
+        probe.IOFile.__init__(self, shn=shn, subdir="AUG")
 
 
-class ProbeAUG(Probe):
+class ProbeAUG(probe.Probe):
     def __init__(self, shn=27695, sock=None):
-        Probe.__init__(self, shn, sock)
+        probe.Probe.__init__(self, shn, sock)
 
         self.IO_mds = IOMdsAUG(shn, sock)
         self.IO_file = IOFileAUG(shn)
@@ -30,12 +31,21 @@ class ProbeAUG(Probe):
         self.R = x['VOL3']
 
         self.V = x['VOL1']
-        self.I = np.c_[x['CUR1'], x['CUR2']]
+        self.I = np.r_[x['CUR1'], x['CUR2']].reshape((2,-1))
 
-        self.I_offs = np.mean(self.I[:100,:],0)
+        self.I_offs = np.mean(self.I[:,:100],1)
 
+        self.R -= self.R[0]
         self.V = -self.V
-        self.I = self.I_offs - self.I
+        self.I = self.I_offs[:,None] - self.I
+
+        R = probe.PositionSignal(self.t, self.R, 'R')
+        V = probe.VoltageSignal(self.t, self.V, 'V')
+
+        self.S = {'R': R,
+                  'V': V,
+                  'I1': probe.CurrentSignal(self.t, self.I[0], 'I1', V),
+                  'I2': probe.CurrentSignal(self.t, self.I[1], 'I2', V)}
 
 
 
