efbd0770d276199d4d0ed4b2ceb438f2dd82cb8f
smueller <smueller@dec913bc-ca15-4b49-ae4c-4c4ad41e7e36>
2012-10-02 10:33:39 +0000

    - config_xpr2.py: Move Tip, CylindricalTip, Head and Shot2 to config.py,   where Shot2 is the default ShotClass for Experiment. Thus config_xpr2.py   can use the vanilla Campaign class.
    git-svn-id: file:///svn_repos/pytokamak.new@45 dec913bc-ca15-4b49-ae4c-4c4ad41e7e36

--- python/config.py
+++ python/config.py
@@ -4,6 +4,28 @@ from collections import OrderedDict
 
 from probe import PositionSignal, VoltageSignal, CurrentSignal
 
+class Tip:
+    def __init__(self, area, proj_area, pos, V_keys=None, I_keys=None):
+        self.area = area
+        self.proj_area = proj_area
+        self.pos = pos
+        self.keys = dict(V=V_keys, I=I_keys)
+
+
+class CylindricalTip(Tip):
+    def __init__(self, r, z, *args, **kw):
+        self.r, self.z = r, z
+        area = 2.*np.pi*r*z
+        proj_area = 2*r*z
+        Tip.__init__(self, area, proj_area, *args, **kw)
+
+
+class Head:
+    def __init__(self, tips, R_keys=None):
+        self.tips = tips
+        self.keys = dict(R=R_keys)
+
+
 class Map:
     def __init__(self, R, VI=None, V=None, I=None):
         self.R = R
@@ -99,8 +121,84 @@ class Shot:
                 V *= ampV
 
 
+class Shot2:
+    def __init__(self, comment="", expt=None, shn=None, dig=None,
+            head=None, amp_default=None, lines=None, **kw):
+        self.comment = comment
+        self.expt = expt
+        self.shn = shn
+        self.dig = dig
+        self.head = head
+
+        self.amp_default = amp_default.copy()
+        for k in self.amp_default.iterkeys():
+            try:
+                self.amp_default[k] = kw.pop(k)
+            except KeyError:
+                pass
+
+        self.lines = lines
+    
+    def copy(self, comment="", expt=None, shn=None, **kw):
+        if expt is None: 
+            expt = self.expt
+        if shn is None:
+            shn = self.shn
+        return self.__class__(comment, expt, shn, self.dig, 
+                              self.head, self.amp_default, self.lines, **kw)
+
+    def get(self, line, what, key):
+        if key is None:
+            return None
+        elif what == 'amp':
+            return self.lines[line]['amp'].get(key, self.amp_default[key])
+        else:
+            return self.lines[line][what][key]
+
+    def all_keys(self):
+        keys = [self.head.keys['R']]
+        for tip in self.head.tips:
+            keys.extend([tip.keys['V'], tip.keys['I']])
+        return keys
+
+    def unique_keys(self):
+        unique_keys = np.unique(self.all_keys())
+        if unique_keys[0] is None:
+            unique_keys = unique_keys[1:]
+        return list(unique_keys)
+
+    def __repr__(self):
+        return "%d: %s" % (self.shn, self.comment)
+
+    def mapsig(self, x, line):
+        self.unique_sigs = {k: x[self.get(line, 'mapping', k)].astype('d') 
+                for k in self.unique_keys()}
+
+        t = x['t']
+        R = self.unique_sigs[self.head.keys['R']]
+        S = dict(R=PositionSignal(R, t, name='R'))
+
+        for i, tip in enumerate(self.head.tips, start=1):
+            keyV, keyI = tip.keys['V'], tip.keys['I']
+            if keyV is None:
+                V = None
+            else:
+                V = VoltageSignal(self.unique_sigs[keyV], t, name='V%d' % i)
+            if keyI is None:
+                S[i] = V
+            else:
+                S[i] = CurrentSignal(self.unique_sigs[keyI], t, V, name='I%d' % i)
+
+        return S
+
+    def calib(self, S, line):
+        for k in self.unique_sigs.iterkeys():
+            amp = self.get(line, 'amp', k)
+            amp.apply(self.unique_sigs[k])
+
+
 class Experiment:
-    def __init__(self, date=None, campaign=None, ShotClass=Shot):
+    def __init__(self, date=None, campaign=None, ShotClass=Shot2):
         self.date, self.campaign, self.ShotClass = date, campaign, ShotClass
         self.x = OrderedDict()
 
@@ -110,8 +208,8 @@ class Experiment:
     def add(self, shn, *args, **kw):
         self.x[shn] = self.ShotClass(*args, expt=self, shn=shn, **kw)
         
-    def rep(self, shn, shn0, comment=""):
-        self.x[shn] = self.x[shn0].copy(comment, shn=shn)
+    def rep(self, shn, shn0, comment="", **kw):
+        self.x[shn] = self.x[shn0].copy(comment, shn=shn, **kw)
 
     @property
     def shots(self):
--- python/config_xpr2.py
+++ python/config_xpr2.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-from config import Experiment, Campaign
+from config import CylindricalTip, Head, Campaign
 
-from probe import Amp, PositionSignal, VoltageSignal, CurrentSignal
+from probe import Amp
 
 
 ampUnity = Amp(fact=1.)
@@ -49,41 +49,16 @@ CurrentProbe3 = {
     50: Amp(fact=0.5*50/10)}
 
 
-class Tip:
-    def __init__(self, area, proj_area, pos, V_links=None, I_links=None):
-        self.area = area
-        self.proj_area = proj_area
-        self.pos = pos
-        self.links = dict(V=V_links, I=I_links)
-
-    def get_V(self, line, field):
-        return None
-
-
-class CylindricalTip(Tip):
-    def __init__(self, r, z, *args, **kw):
-        self.r, self.z = r, z
-        area = 2.*np.pi*r*z
-        proj_area = 2*r*z
-        Tip.__init__(self, area, proj_area, *args, **kw)
-
-
-class Head:
-    def __init__(self, tips, R_links=None):
-        self.tips = tips
-        self.links = dict(R=R_links)
-
-
 class TipXPR(CylindricalTip):
     def __init__(self, *args, **kw):
         CylindricalTip.__init__(self, 0.0005, 0.003, *args, **kw)
 
 
-tip1 = TipXPR(pos='lower left', V_links='ampV', I_links='ampI1')
-tip2 = TipXPR(pos='lower right', V_links='ampV', I_links='ampI2')
-tip3 = TipXPR(pos='upper', V_links='ampVF', I_links=None)
+tip1 = TipXPR(pos='lower left', V_keys='ampV', I_keys='ampI1')
+tip2 = TipXPR(pos='lower right', V_keys='ampV', I_keys='ampI2')
+tip3 = TipXPR(pos='upper', V_keys='ampVF', I_keys=None)
 
-head = Head(tips=(tip1, tip2, tip3), R_links='ampR')
+head = Head(tips=(tip1, tip2, tip3), R_keys='ampR')
 
 
 amp_default = dict(
@@ -125,93 +100,7 @@ lines = dict(XPR=lines_XPR, LPS=lines_LPS)
 
 def_LPS = dict(dig='LPS', head=head, amp_default=amp_default, lines=dict(LPS=lines_LPS))
 
-class Shot2:
-    def __init__(self, comment="", expt=None, shn=None, dig=None,
-            head=None, amp_default=None, lines=None, **kw):
-        self.comment = comment
-        self.expt = expt
-        self.shn = shn
-        self.dig = dig
-        self.head = head
-
-        self.amp_default = amp_default.copy()
-        for k in self.amp_default.keys():
-            try:
-                self.amp_default[k] = kw.pop(k)
-            except KeyError:
-                pass
-
-        self.lines = lines
-    
-    def copy(self, comment="", expt=None, shn=None):
-        if expt is None: 
-            expt = self.expt
-        if shn is None:
-            shn = self.shn
-        return self.__class__(comment, expt, shn, self.dig, 
-                              self.head, self.amp_default, self.lines)
-
-    def get(self, line, what, key):
-        if key is None:
-            return None
-        elif what == 'amp':
-            return self.lines[line]['amp'].get(key, self.amp_default[key])
-        else:
-            return self.lines[line][what][key]
-
-    def all_keys(self):
-        keys = [self.head.links['R']]
-        for tip in self.head.tips:
-            keys.extend([tip.links['V'], tip.links['I']])
-        return keys
-
-    def unique_keys(self):
-        unique_keys = np.unique(self.all_keys())
-        if unique_keys[0] is None:
-            unique_keys = unique_keys[1:]
-        return list(unique_keys)
-
-    def __repr__(self):
-        return "%d: %s" % (self.shn, self.comment)
-
-    def mapsig(self, x, line):
-        self.unique_sigs = {k: x[self.get(line, 'mapping', k)].astype('d') 
-                for k in self.unique_keys()}
-
-        t = x['t']
-        R = self.unique_sigs[self.head.links['R']]
-        S = dict(R=PositionSignal(R, t, name='R'))
-
-        for i, tip in enumerate(self.head.tips, start=1):
-            keyV, keyI = tip.links['V'], tip.links['I']
-            if keyV is None:
-                V = None
-            else:
-                V = VoltageSignal(self.unique_sigs[keyV], t, name='V%d' % i)
-            if keyI is None:
-                S[i] = V
-            else:
-                S[i] = CurrentSignal(self.unique_sigs[keyI], t, V, name='I%d' % i)
-
-        return S
-
-    def calib(self, S, line):
-        for k in self.unique_sigs.iterkeys():
-            amp = self.get(line, 'amp', k)
-            amp.apply(self.unique_sigs[k])
-
-
-class ExperimentAUG(Experiment):
-    def __init__(self, *args, **kw):
-        Experiment.__init__(self, *args, ShotClass=Shot2, **kw)
-
-
-class CampaignAUG(Campaign):
-    def __init__(self, *args, **kw):
-        Campaign.__init__(self, *args, ExperimentClass=ExperimentAUG, **kw)
-
-
-campaign = CampaignAUG()
+campaign = Campaign()
 
 
 ############################################
@@ -224,23 +113,22 @@ E.add(27684, "",
 E.rep(27685, 27684)
 E.rep(27686, 27684, "Both current probes on tip 1")
 
-E.add(27687, "Changed direction of 2nd current probe",
-             ampI1 = ampInv*CurrentProbe1[10]*Preamp1[5],
-             ampI2 = CurrentProbe2[10]*Preamp2[5], **def_LPS)
+E.rep(27687, 27686, "Changed direction of 2nd current probe",
+             ampI2 = CurrentProbe2[10]*Preamp2[5])
 
 E.rep(27688, 27687)
 E.rep(27689, 27687, "First shot of Leena's experiment")
 E.rep(27690, 27687)
 
-E.add(27691, "Current measurement from 10 mA/div to 20 mA/div",
+E.rep(27691, 27690, "Current measurement from 10 mA/div to 20 mA/div",
              ampI1 = ampInv*CurrentProbe1[20]*Preamp1[5],
-             ampI2 = CurrentProbe2[20]*Preamp2[5], **def_LPS)
+             ampI2 = CurrentProbe2[20]*Preamp2[5])
 
 E.rep(27692, 27691, "All the way through, but signals saturated")
 
-E.add(27693, "Current probes 50 mA/div",
+E.rep(27693, 27692, "Current probes 50 mA/div",
              ampI1 = ampInv*CurrentProbe1[50]*Preamp1[5],
-             ampI2 = CurrentProbe2[50]*Preamp2[5], **def_LPS)
+             ampI2 = CurrentProbe2[50]*Preamp2[5])
 
 E.rep(27694, 27693, "HWM resumed experiment")
 E.rep(27695, 27693, "Calibration after this shot")
