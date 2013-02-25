ecec009af8d901c56dd61a8e1449a8557ebbe030
smueller <smueller@dec913bc-ca15-4b49-ae4c-4c4ad41e7e36>
2013-02-25 20:22:52 +0000

    - fitfunmodule.c: Generate fit functions from #define templates.
    git-svn-id: file:///svn_repos/pytokamak.new@200 dec913bc-ca15-4b49-ae4c-4c4ad41e7e36

--- fitfun/python/fitfunmodule.c
+++ fitfun/python/fitfunmodule.c
@@ -5,230 +5,96 @@
 
 #include <minpack.h>
 
-void e2(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y;
-    double P0 = P[0], P1 = P[1];
-
-    for (i=D->m; i--; ) {
-        *y++ = P0*exp(-P1*(*x++));
-    }
+#define template(fun, defs, body, expr)                        \
+void fun(data *D) {                                            \
+    int i;                                                     \
+    double *P = D->P, *x = D->x, *y = D->y;                    \
+    double defs;                                               \
+    for (i=D->m; i--; ) {                                      \
+        body                                                   \
+        *y++ = expr;                                           \
+    }                                                          \
 }
 
-void e2_diff(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *ydata = D->ydata;
-    double P0 = P[0], P1 = P[1];
-
-    for (i=D->m; i--; ) {
-        *y++ = P0*exp(-P1*(*x++)) - *ydata++;
-    }
+#define template_diff(fun, defs, body, expr)                   \
+void fun##_diff(data *D) {                                     \
+    int i;                                                     \
+    double *P = D->P, *x = D->x, *y = D->y, *ydata = D->ydata; \
+    double defs;                                               \
+    for (i=D->m; i--; ) {                                      \
+        body                                                   \
+        *y++ = expr - *ydata++;                                \
+    }                                                          \
 }
 
-double e2_rms(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y;
-    double P0 = P[0], P1 = P[1];
-    double dy2 = 0., dy;
-
-    for (i=D->m; i--; ) {
-        dy = P0*exp(-P1*(*x++)) - *y++;
-        dy2 += dy*dy;
-    }
-    return sqrt(dy2) / D->m;
+#define template_rms(fun, defs, body, expr)                    \
+double fun##_rms(data *D) {                                    \
+    int i;                                                     \
+    double dy2 = 0., dy;                                       \
+    double *P = D->P, *x = D->x, *y = D->y;                    \
+    double defs;                                               \
+    for (i=D->m; i--; ) {                                      \
+        body                                                   \
+        dy = expr - *y++;                                      \
+        dy2 += dy*dy;                                          \
+    }                                                          \
+    return sqrt(dy2) / D->m;                                   \
 }
 
-void IV3(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
 
-    for (i=D->m; i--; ) {
-        *y++ = P0*(1. - exp((*x++ - P1)*iP2));
-    }
-}
+#define EMPTY
 
-void IV3_diff(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *ydata = D->ydata;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
+#define e2_defs P0 = P[0], P1 = P[1]
+#define e2_expr P0*exp(-P1*(*x++))
 
-    for (i=D->m; i--; ) {
-        *y++ = P0*(1. - exp((*x++ - P1)*iP2)) - *ydata++;
-    }
-}
+template(e2, e2_defs, EMPTY, e2_expr)
+template_diff(e2, e2_defs, EMPTY, e2_expr)
+template_rms(e2, e2_defs, EMPTY, e2_expr)
 
-double IV3_rms(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
-    double dy2 = 0., dy;
 
-    for (i=D->m; i--; ) {
-        dy = P0*(1. - exp((*x++ - P1)*iP2)) - *y++;
-        dy2 += dy*dy;
-    }
-    return sqrt(dy2) / D->m;
-}
+#define IV3_defs P0 = P[0], P1 = P[1], iP2 = 1./P[2]
+#define IV3_expr P0*(1. - exp((*x++ - P1)*iP2))
 
-void IV4(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
-    double dP0 = P[3]-P0;
-    double ai, P0i;
+template(IV3, IV3_defs, EMPTY, IV3_expr)
+template_diff(IV3, IV3_defs, EMPTY, IV3_expr)
+template_rms(IV3, IV3_defs, EMPTY, IV3_expr)
 
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        *y++ = P0i*(1. - exp((*x++ - P1)*iP2));
-    }
-}
 
-void IV4_diff(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a, *ydata = D->ydata;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
-    double dP0 = P[3]-P0;
-    double ai, P0i;
+#define IV4_defs *a = D->a, P0 = P[0], P1 = P[1], iP2 = 1./P[2], \
+                 dP0 = P[3]-P0, ai, P0i
+#define IV4_body ai = *a++; \
+                 P0i = P0 + ai*dP0;
+#define IV4_expr P0i*(1. - exp((*x++ - P1)*iP2))
 
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        *y++ = P0i*(1. - exp((*x++ - P1)*iP2)) - *ydata++;
-    }
-}
+template(IV4, IV4_defs, IV4_body, IV4_expr)
+template_diff(IV4, IV4_defs, IV4_body, IV4_expr)
+template_rms(IV4, IV4_defs, IV4_body, IV4_expr)
 
-double IV4_rms(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
-    double dP0 = P[3]-P0;
-    double ai, P0i;
-    double dy2 = 0., dy;
 
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        dy = P0i*(1. - exp((*x++ - P1)*iP2)) - *y++;
-        dy2 += dy*dy;
-    }
-    return sqrt(dy2) / D->m;
-}
+#define IV5_defs *a = D->a, P0 = P[0], P1 = P[1], iP2 = 1./P[2], \
+                 dP0 = P[3]-P0, dP1 = P[4]-P1, ai, P0i, P1i
+#define IV5_body ai = *a++; \
+                 P0i = P0 + ai*dP0; \
+                 P1i = P1 + ai*dP1;
+#define IV5_expr P0i*(1. - exp((*x++ - P1i)*iP2))
 
-void IV5(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
-    double dP0 = P[3]-P0, dP1 = P[4]-P1;
-    double ai, P0i, P1i;
+template(IV5, IV5_defs, IV5_body, IV5_expr)
+template_diff(IV5, IV5_defs, IV5_body, IV5_expr)
+template_rms(IV5, IV5_defs, IV5_body, IV5_expr)
 
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        P1i = P1 + ai*dP1;
-        *y++ = P0i*(1. - exp((*x++ - P1i)*iP2));
-    }
-}
 
-void IV5_diff(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a, *ydata = D->ydata;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
-    double dP0 = P[3]-P0, dP1 = P[4]-P1;
-    double ai, P0i, P1i;
+#define IV6_defs *a = D->a, P0 = P[0], P1 = P[1], P2 = P[2], \
+                 dP0 = P[3]-P0, dP1 = P[4]-P1, dP2 = P[5]-P2, \
+                 ai, P0i, P1i, P2i
+#define IV6_body ai = *a++; \
+                 P0i = P0 + ai*dP0; \
+                 P1i = P1 + ai*dP1; \
+                 P2i = P2 + ai*dP2;
+#define IV6_expr P0i*(1. - exp((*x++ - P1i)/P2i))
 
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        P1i = P1 + ai*dP1;
-        *y++ = P0i*(1. - exp((*x++ - P1i)*iP2)) - *ydata++;
-    }
-}
-
-double IV5_rms(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2];
-    double dP0 = P[3]-P0, dP1 = P[4]-P1;
-    double ai, P0i, P1i;
-    double dy2 = 0., dy;
-
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        P1i = P1 + ai*dP1;
-        dy = P0i*(1. - exp((*x++ - P1i)*iP2)) - *y++;
-        dy2 += dy*dy;
-    }
-    return sqrt(dy2) / D->m;
-}
-
-void IV6(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a;
-    double P0 = P[0], P1 = P[1], P2 = P[2];
-    double dP0 = P[3]-P0, dP1 = P[4]-P1, dP2 = P[5]-P2;
-    double ai, P0i, P1i, P2i;
-
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        P1i = P1 + ai*dP1;
-        P2i = P2 + ai*dP2;
-        *y++ = P0i*(1. - exp((*x++ - P1i)/P2i));
-    }
-}
-
-void IV6_diff(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a, *ydata = D->ydata;
-    double P0 = P[0], P1 = P[1], P2 = P[2];
-    double dP0 = P[3]-P0, dP1 = P[4]-P1, dP2 = P[5]-P2;
-    double ai, P0i, P1i, P2i;
-
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        P1i = P1 + ai*dP1;
-        P2i = P2 + ai*dP2;
-        *y++ = P0i*(1. - exp((*x++ - P1i)/P2i)) - *ydata++;
-    }
-}
-
-double IV6_rms(data *D)
-{
-    int i;
-    double *P = D->P, *x = D->x, *y = D->y, *a = D->a;
-    double P0 = P[0], P1 = P[1], P2 = P[2];
-    double dP0 = P[3]-P0, dP1 = P[4]-P1, dP2 = P[5]-P2;
-    double ai, P0i, P1i, P2i;
-    double dy2 = 0., dy;
-
-    for (i=D->m; i--; ) {
-        ai = *a++;
-        P0i = P0 + ai*dP0;
-        P1i = P1 + ai*dP1;
-        P2i = P2 + ai*dP2;
-        dy = P0i*(1. - exp((*x++ - P1i)/P2i)) - *y++;
-        dy2 += dy*dy;
-    }
-    return sqrt(dy2) / D->m;
-}
+template(IV6, IV6_defs, IV6_body, IV6_expr)
+template_diff(IV6, IV6_defs, IV6_body, IV6_expr)
+template_rms(IV6, IV6_defs, IV6_body, IV6_expr)
 
 
 double pow_075(double x) 
@@ -240,53 +106,15 @@ double pow_075(double x)
     return (tmp + 2.)*sqrt(tmp - 1.);
 }
 
-void IVdbl(data *D)
-{
-    int i;
-    double arg, exp_arg;
-    double *P = D->P, *x = D->x, *y = D->y;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2], P3 = P[3], P4 = P[4];
-    double A = P3, B = P4;
 
-    for (i=D->m; i--; ) {
-        arg = (P1 - *x++)*iP2;
-        exp_arg = exp(arg);
-        *y++ = P0*(exp_arg - 1. - A*pow_075(arg)) / (exp_arg + B);
-    }
-}
+#define IVdbl_defs P0 = P[0], P1 = P[1], iP2 = 1./P[2], P3 = P[3], P4 = P[4], \
+                   A = P3, B = P4, arg, exp_arg
+#define IVdbl_body arg = (P1 - *x++)*iP2; exp_arg = exp(arg);
+#define IVdbl_expr P0*(exp_arg - 1. - A*pow_075(arg)) / (exp_arg + B)
 
-void IVdbl_diff(data *D)
-{
-    int i;
-    double arg, exp_arg;
-    double *P = D->P, *x = D->x, *y = D->y, *ydata = D->ydata;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2], P3 = P[3], P4 = P[4];
-    double A = P3, B = P4;
-
-    for (i=D->m; i--; ) {
-        arg = (P1 - *x++)*iP2;
-        exp_arg = exp(arg);
-        *y++ = P0*(exp_arg - 1. - A*pow_075(arg)) / (exp_arg + B) - *ydata++;
-    }
-}
-
-double IVdbl_rms(data *D)
-{
-    int i;
-    double arg, exp_arg;
-    double *P = D->P, *x = D->x, *y = D->y;
-    double P0 = P[0], P1 = P[1], iP2 = 1./P[2], P3 = P[3], P4 = P[4];
-    double A = P3, B = P4;
-    double dy2 = 0., dy;
-
-    for (i=D->m; i--; ) {
-        arg = (P1 - *x++)*iP2;
-        exp_arg = exp(arg);
-        dy = P0*(exp_arg - 1. - A*pow_075(arg)) / (exp_arg + B) - *y++;
-        dy2 += dy*dy;
-    }
-    return sqrt(dy2) / D->m;
-}
+template(IVdbl, IVdbl_defs, IVdbl_body, IVdbl_expr)
+template_diff(IVdbl, IVdbl_defs, IVdbl_body, IVdbl_expr)
+template_rms(IVdbl, IVdbl_defs, IVdbl_body, IVdbl_expr)
 
 
 #define fit_template(name)   \
