- Fix several bugs that occurred in unusual cases:    * LP/sig.py: Signal: Remove units check in overloaded operators,      since units are not property implemented anyway at this point.    * LP/sig.py: PositionSignal: get_crossings(): Make sure no plunge      is found if just the digital position signal jumped by 1 bit.      plunge_mask.py: Properly handle the case where no plunge was      found.    * LP/probe_xpr.py, LP/probe.py: plot_separatrix_crossings: Capture      all exceptions, so that it never blocks execution if something      goes wrong. - LP/probe_xpr.py: calib(): Experiment with masking arcing signals,   but deactivate, since masks are not carried through to the end   anyway. - LP/config_xpr_common.py: Add comment to CurrentProbe, explaining   how their calibration works.
git-svn-id: file:///svn_repos/pytokamak.new@308 dec913bc-ca15-4b49-ae4c-4c4ad41e7e36

LP/probe_xpr.py
@@ -140,7 +140,9 @@ class ProbeXPR(Probe):
     @memoized_property
     def _psi_spl(self):
         R, z = self.pos.x.T
-        Ri = np.linspace(R.min(), R.max(), 20)
+        Rm, RM = R.min(), R.max()
+        if Rm == RM: Rm -= 0.01
+        Ri = np.linspace(Rm, RM, 20)
         zi = np.array([self.z0]).repeat(Ri.size)
         return self.eqi.get_path_spline(Ri, Ri, zi)
 
@@ -170,6 +172,14 @@ class ProbeXPR(Probe):
         S['Rs'] = S['R'].copy().mediansmooth(100)
         S['tip1+tip2'] = S['tip1'] + S['tip2']
         S['tip1+tip2'].label = 'Mach tips sum'
+        
+        """
+        if not S['tip1+tip2'].V.is_swept:
+            mask = (S['tip1+tip2'].V > -150.) | (S['tip1+tip2'] > 1.9)
+            S['tip1+tip2'] = S['tip1+tip2'].masked(mask)
+            S['tip1'] = S['tip1'].masked(mask)
+            S['tip2'] = S['tip2'].masked(mask)
+        """
 
     def calc_res(self, ID='IV'):
         tips = self.head.tips
@@ -210,7 +220,7 @@ class ProbeXPR(Probe):
         i, meas = self.PP_meas.eval()
 
         dtype = zip(keys, [np.double]*len(keys))
-        meas = meas.view(dtype).reshape(-1).view(np.recarray)
+        meas = meas.view(dtype).ravel().view(np.recarray)
 
         return PhysicalResults(self, i, meas)
 
@@ -241,7 +251,7 @@ class ProbeXPR(Probe):
                 for ax in axes:
                     for xlim in xsep:
                         vrect(ax, xlim, **kw)
-            except ValueError:
+            except:
                 warn("Could not generate patches, falling back to lines")
                 Probe.plot_separatrix_crossings(self, axes, **kw)
         elif sepmode == 'lines':

LP/sig.py
@@ -672,8 +672,8 @@ class Signal:
     def _op_factory(op, calcfun):
         def apply(self, other, out=None):
             try:
-                if other.units != self.units:
-                    raise Exception("Unit mismatch")
+                #if other.units != self.units:
+                #    raise Exception("Unit mismatch")
                 other = other.x
             except AttributeError:
                 pass
@@ -1037,7 +1037,7 @@ class PositionSignal(Signal):
 
     def get_crossings(self):
         x = self.get_baseline()
-        x0, xM = x.mean(), self.x.max()
+        x0, xM = x.mean(), max(self.x.max(), 0.)
         lvl = x0 + self.lvl_fact*(xM - x0)
         return self.crossings(lvl, self.dist_threshold)
 
@@ -1079,8 +1079,11 @@ class PositionSignal(Signal):
 
     def plunge_mask(self, i, plunge=None, inout=None):
         w = self.plunges(plunge, inout)
-        ind0, ind1 = np.searchsorted(i, w)
-        return np.concatenate(map(np.arange, ind0, ind1))
+        if w.shape[1] == 0:
+            return np.arange(0)
+        else:
+            ind0, ind1 = np.searchsorted(i, w)
+            return np.concatenate(map(np.arange, ind0, ind1))
 
     def plot_plunges(self, ax=None, **kw):
         ax = Signal.plot(self, ax, **kw)